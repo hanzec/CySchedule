9abae4ee61072414e1dbffd5e006630f17df3721
Hanze Chen <hanzech@iastate.edu>
2019-09-09 18:10:42 -0500

    springbook experiment update

---
+++ Experiments/SpringBootExperiment/src/main/java/edu/iastate/coms309/springbootexperiment/SpringbootexperimentApplication.java
@@ -0,0 +1,14 @@
+package edu.iastate.coms309.springbootexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootexperimentApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringbootexperimentApplication.class, args);
+    }
+
+}
---
+++ Experiments/SpringBootExperiment/src/test/java/edu/iastate/coms309/springbootexperiment/SpringbootexperimentApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.iastate.coms309.springbootexperiment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootexperimentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
