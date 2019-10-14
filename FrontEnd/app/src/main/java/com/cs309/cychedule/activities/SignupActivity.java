975f98665a9519d675722bd0892df2c788b7fff4
Xingting Lai <xlai@iastate.edu>
2019-10-14 17:23:23 -0500

    modify in signup and login

--- FrontEnd/app/src/main/java/com/cs309/cychedule/activities/SignupActivity.java
+++ FrontEnd/app/src/main/java/com/cs309/cychedule/activities/SignupActivity.java
@@ -18,33 +18,30 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import com.android.volley.toolbox.Volley;
 import com.cs309.cychedule.R;
 import com.cs309.cychedule.utilities.userUtil;
 import com.cs309.cychedule.patterns.Singleton;
-
-import javax.xml.transform.ErrorListener;
+import com.google.gson.JsonObject;
+import com.google.gson.internal.LinkedHashTreeMap;
 
 public class SignupActivity extends AppCompatActivity {
 
-
-   //private static String URL_SIGNUP = "http://10.26.187.17:8080/api/v1/auth/register?username=N22L&password=fuck&email=nononono@fucker.edu&firstname=yello&lastname=mello";
-        private static String URL_SIGNUP = "https://dev.hanzec.com/api/v1/auth/register";
+    private static String URL_SIGNUP = "https://dev.hanzec.com/api/v1/auth/register";
     private static final String TAG = "SignupActivity";
 
     @BindView(R.id.input_userName) EditText _userNameText;
@@ -55,7 +52,7 @@ public class SignupActivity extends AppCompatActivity {
     @BindView(R.id.input_reEnterPassword) EditText _reEnterPasswordText;
     @BindView(R.id.btn_signup) Button _signupButton;
     @BindView(R.id.link_login) TextView _loginLink;
-    
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +64,11 @@ public class SignupActivity extends AppCompatActivity {
         {
             @Override
             public void onClick(View v) {
-                signup();
+                try {
+                    signup();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
         });
 
@@ -75,23 +76,20 @@ public class SignupActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Finish the registration screen and return to the Login activity
-                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
                 finish();
+                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
     }
 
-
-
-    public void signup() {
+    public void signup() throws JSONException {
         Log.d(TAG, "Signup");
 
-//        if (!validate()) {
-//            onSignupFailed();
-//            return;
-//        }
+        if (!validate()) {
+            onSignupFailed();
+        }
 
         _signupButton.setEnabled(false);
 
@@ -101,30 +99,46 @@ public class SignupActivity extends AppCompatActivity {
         progressDialog.setMessage("Creating Account...");
         progressDialog.show();
 
-        final String userName = this._userNameText.getText().toString();
-        final String password = this._passwordText.getText().toString();
-        final String reEnterPassword = this._reEnterPasswordText.getText().toString();
-        final String email = this._emailText.getText().toString();
-        final String firstName = this._firstNameText.getText().toString();
-        final String lastName = this._lastNameText.getText().toString();
-        
-    //        _signupButton.setVisibility(View.GONE);
+        final String userName = _userNameText.getText().toString().trim();
+        final String password = _passwordText.getText().toString().trim();
+        final String reEnterPassword = _reEnterPasswordText.getText().toString().trim();
+        final String email = _emailText.getText().toString().trim();
+        final String firstName = _firstNameText.getText().toString().trim();
+        final String lastName = _lastNameText.getText().toString().trim();
+//
+//        // TODO: Implement your own signup logic here.
+//
+//        Log.d("Inputs:", userName);
+//        System.out.println(userName + password + email + firstName + lastName);
+        _signupButton.setVisibility(View.GONE);
 
-        //final String userName = this._userNameText.toString().trim();
-        //final String password = this._passwordText.toString().trim();
+        RequestQueue requestQueue = Singleton.getInstance(this.getApplicationContext()).getRequestQueue();
+        //RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.start();
 
-//        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_SIGNUP,
-//                new Response.Listener<String>()
+//        JSONObject jsonObject = new JSONObject();
+//        try
+//        {
+//            jsonObject.put("userName", userName);
+//            jsonObject.put("password", password);
+//            jsonObject.put("email", email);
+//            jsonObject.put("firstName", firstName);
+//            jsonObject.put("lastName", lastName);
+//        }
+//        catch (JSONException e)
+//        {
+//            e.printStackTrace();
+//        }
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL_SIGNUP, jsonObject,
+//                new Response.Listener<JSONObject>()
 //                {
 //                    @Override
-//                    public void onResponse(String response)
+//                    public void onResponse(JSONObject response)
 //                    {
 //                        try
 //                        {
-//                            JSONObject jsonObject = new JSONObject(response);
-//                            String success = jsonObject.getString("success");
-//
-//                            if (success.equals("1"))
+//                            String status = response.getString("status");
+//                            if (status.equals("201"))
 //                            {
 //                                Toast.makeText(SignupActivity.this, "Register Success!", Toast.LENGTH_SHORT).show();
 //                            }
@@ -144,167 +158,72 @@ public class SignupActivity extends AppCompatActivity {
 //                    {
 //                        Toast.makeText(SignupActivity.this, "Register Error! " + error.toString(), Toast.LENGTH_SHORT).show();
 //                        _signupButton.setVisibility(View.VISIBLE);
+//                        Log.d("Error", error.toString());
 //                    }
 //                }
-//
 //        )
-//
 //        {
 //            @Override
-//            protected Map<String, String> getParams() throws AuthFailureError
-//            {
-//                Map<String, String> params = new HashMap<>();
-//                params.put("userName", userName);
-//                params.put("password", password);
-//                params.put("email", email);
-//                params.put("firstName", firstName);
-//                params.put("lastName", lastName);
-//                return super.getParams();
+//            public int getMethod() {
+//                return Method.POST;
+//            }
+//
+//            @Override
+//            public Priority getPriority() {
+//                return Priority.NORMAL;
 //            }
 //        };
+//        Singleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+        //requestQueue.add(jsonObjectRequest);
 
-
-
-        /**GET*/
-//        StringRequest stringRequest = new StringRequest(URL_SIGNUP,
-//                new Response.Listener<String>() {
-//                    @Override
-//                    public void onResponse(String response) {
-//                        Log.d("TAG", response);
-//                        Toast.makeText(SignupActivity.this, response , Toast.LENGTH_SHORT).show();
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                Log.e("TAG", error.getMessage(), error);
-//            }
-//        }
-//        );
-
-
-        /**MAP*/
-            //
-            // RequestQueue requestQueue = Volley.newRequestQueue(this);
-            // requestQueue.start();
-            // StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_SIGNUP,
-            //         new Response.Listener<String>() {
-            //             @Override
-            //             public void onResponse(String response) {
-            //                 Log.d("TAG", response);
-            //                 Toast.makeText(SignupActivity.this, response, Toast.LENGTH_SHORT).show();
-            //             }
-            //         }, new Response.ErrorListener() {
-            //     @Override
-            //     public void onErrorResponse(VolleyError error) {
-            //         Log.e("TAG", error.getMessage(), error);
-            //     }
-            // }) {
-            //     @Override
-            //     protected Map<String, String> getParams() throws AuthFailureError {
-            //         Map<String, String> map = new HashMap<String, String>();
-            //         map.put("userame", "111111111111111");
-            //         map.put("password", "WOOHOO");
-            //         map.put("email", "FOO");
-            //         map.put("firstName", "BAR");
-            //         map.put("lastName", "BUGS");
-            //         return map;
-            //     }
-            // };
-            //
-            // requestQueue.add(stringRequest);
-
-/**V3*/
-
-       RequestQueue rq = Volley.newRequestQueue(this.getApplicationContext());
-rq.start();
-       JSONObject params = new JSONObject();
-       try {
-           params.put("usename", "LAHEE");
-           params.put("password", "YAHOO");
-           params.put("email", "YAHOO");
-           params.put("firstName", "YAHOO");
-           params.put("lastName", "YAHOO");
-       } catch (JSONException e) {
-           e.printStackTrace();
-       }
-
-       JsonObjectRequest thisIsSth = new JsonObjectRequest(Request.Method.POST,
-               URL_SIGNUP, params, //Not null???
-               new Response.Listener<JSONObject>() {
-
-                   @Override
-                   public void onResponse(JSONObject response) {
-                       Log.d(TAG, response.toString());
-                       // pDialog.hide();
-                   }
-               }, new Response.ErrorListener() {
-
-           @Override
-           public void onErrorResponse(VolleyError error) {
-               VolleyLog.d(TAG, "WTF? : " + error.getMessage());
-               //pDialog.hide();
-           }
-       });
-
-// Adding request to request queue
-       rq.add(thisIsSth);
-
-/**JSOBJ*/
-//         try {
-//             RequestQueue requestQueue = Volley.newRequestQueue(this);
-//             JSONObject jsonBody = new JSONObject();
-//             jsonBody.put("NMSL", "NMSSSSSSSSL");
-//             jsonBody.put("NMSL", "YAHOO");
-//             final String requestBody = jsonBody.toString();
-//             //NMSL&password=peach&email=nononono@lemon.edu&firstname=yello&lastname=mello";
-//             URL_SIGNUP+=userName;
-//             URL_SIGNUP+="&password=";
-//             URL_SIGNUP+=password;
-//             URL_SIGNUP+="&email=";
-//             URL_SIGNUP+=email;
-//             URL_SIGNUP+="&firstname=";
-//             URL_SIGNUP+=firstName;
-//             URL_SIGNUP+="&lastname=";
-//             URL_SIGNUP+=lastName;
-//             StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_SIGNUP,
-//                     new Response.Listener<String>() {
-//                 @Override
-//                 public void onResponse(String response) {
-//                     Log.i("VOLLEY", response);
-//
-// //                    if(response.equals("200")){
-// //                        Toast.makeText(SignupActivity.this, "Sign Up Successfully!" , Toast.LENGTH_SHORT).show();
-// //                        response=null;
-// //                    }
-// //                    else{
-//                         Toast.makeText(SignupActivity.this, "Server MSG: "+response , Toast.LENGTH_LONG).show();
-//                         response=null;
-// //                    }
-//                 }
-//             }, new Response.ErrorListener() {
-//                 @Override
-//                 public void onErrorResponse(VolleyError error) {
-//                     Log.e("VOLLEY", error.toString());
-//                 }
-//             }) {
-//                 @Override
-//                 protected Response<String> parseNetworkResponse(NetworkResponse response) {
-//                     String responseString = "";
-//                     if (response != null) {
-//                         responseString = String.valueOf(response.statusCode);
-//                         // can get more details such as response.headers
-//                     }
-//                     return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-//                 }
-//             };
-//
-//             requestQueue.add(stringRequest);
-//         } catch (JSONException e) {
-//             e.printStackTrace();
-//         }
-
-//        RequestQueue requestQueue = Singleton.getInstance(this.getApplicationContext()).getRequestQueue();
-//        Singleton.getInstance(this).addToRequestQueue(stringRequest);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL_SIGNUP,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response)
+                    {
+                        try
+                        {
+                            JSONObject object = new JSONObject(response);
+                            String status = object.getString("status");
+                            if (status.equals("201"))
+                            {
+                                Toast.makeText(SignupActivity.this, "Register Success!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                            Toast.makeText(SignupActivity.this, "Register Error! " + e.toString(), Toast.LENGTH_SHORT).show();
+                            _signupButton.setVisibility(View.VISIBLE);
+                        }
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error)
+                    {
+                        Toast.makeText(SignupActivity.this, "Register Error! " + error.toString(), Toast.LENGTH_SHORT).show();
+                        _signupButton.setVisibility(View.VISIBLE);
+                        Log.d("Error", error.toString());
+                    }
+                }
+        )
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("userName", userName);
+                params.put("password", password);
+                params.put("email", email);
+                params.put("firstName", firstName);
+                params.put("lastName", lastName);
+                return params;
+            }
+        };
+        Singleton.getInstance(this).addToRequestQueue(stringRequest);
+        //requestQueue.add(stringRequest);
 
         new android.os.Handler().postDelayed(
                 new Runnable() {
@@ -312,7 +231,7 @@ rq.start();
                         // On complete call either onSignupSuccess or onSignupFailed
                         // depending on success
                         onSignupSuccess();
-                         onSignupFailed();
+                        // onSignupFailed();
                         progressDialog.dismiss();
                     }
                 }, 3000);
@@ -322,11 +241,15 @@ rq.start();
     public void onSignupSuccess() {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
-        //finish();
+        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
 
     public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Please Check Network!", Toast.LENGTH_LONG).show();
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
         _signupButton.setEnabled(true);
     }
 
@@ -336,7 +259,7 @@ rq.start();
         String userName = _userNameText.getText().toString();
         String password = _passwordText.getText().toString();
         String reEnterPassword = _reEnterPasswordText.getText().toString();
-    
+
         if(Patterns.EMAIL_ADDRESS.matcher(userName).matches() || Patterns.PHONE.matcher(userName).matches()){
             _userNameText.setError(null);
         }
