0dc43527f0ac9209ff334555adf83889c8ba51af
Yizhen <yizhenx@iastate.edu>
2019-10-22 04:21:27 -0500

    main page fix

--- FrontEnd/app/src/main/java/com/cs309/cychedule/activities/ui/calendar/CalendarFragment.java
+++ FrontEnd/app/src/main/java/com/cs309/cychedule/activities/ui/calendar/CalendarFragment.java
@@ -1,6 +1,8 @@
 package com.cs309.cychedule.activities.ui.calendar;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,12 +10,10 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.support.annotation.Nullable;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.cs309.cychedule.R;
@@ -25,9 +25,10 @@ public class CalendarFragment extends Fragment {
     private CalendarViewModel calendarViewModel;
     private Button btnAdd, btnRemvoe;
     private DatePicker datepicker;
-    int year, month, day;
-    String secretText;
-
+    int year, month, day, hour, minute;
+    String eventText, locationText;
+   
+    @RequiresApi(api = Build.VERSION_CODES.M)
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         calendarViewModel = ViewModelProviders.of(this).get(CalendarViewModel.class);
         final View root = inflater.inflate(R.layout.fragment_calendar, container, false);
@@ -40,8 +41,10 @@ public class CalendarFragment extends Fragment {
         // 	}
         // });
 
-        final EditText secretInput = root.findViewById(R.id.et_material_design);
-
+        final EditText eventInput = root.findViewById(R.id.eventTextInput);
+        final EditText locationInput = root.findViewById(R.id.locationTextInput);
+        
+        
         final DatePicker datePicker = root.findViewById(R.id.datepicker_calendar);
         Calendar calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
@@ -55,12 +58,18 @@ public class CalendarFragment extends Fragment {
                 CalendarFragment.this.day = dayOfMonth;
             }
         });
-
+    
+        final TimePicker timePicker = root.findViewById(R.id.timepicker_calendar);
+        timePicker.setIs24HourView(true);
+        
+        
+        
         btnRemvoe = root.findViewById(R.id.removeText_calendar);
         btnRemvoe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                secretInput.setText("");
+                eventInput.setText("");
+                locationInput.setText("");
             }
         });
 
@@ -69,13 +78,23 @@ public class CalendarFragment extends Fragment {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                secretText = secretInput.getText().toString().trim();
-                if (secretText.isEmpty()) {
-                    Toast emptyInputWarning = Toast.makeText(root.getContext(), "Please enter something!", Toast.LENGTH_SHORT);
+                eventText = eventInput.getText().toString();
+                locationText = locationInput.getText().toString();
+                if (eventText.isEmpty()) {
+                    Toast emptyInputWarning = Toast.makeText(root.getContext(), "Please enter the even description!", Toast.LENGTH_SHORT);
                     emptyInputWarning.show();
                 }
                 else {
-                    Snackbar.make(root, "Secret :" + secretText + " @" + year + "." + month + "." + day, Snackbar.LENGTH_LONG)
+                    if(locationText.isEmpty()){
+                        locationInput.setText("Anywhere");
+                    }
+                    locationText = locationInput.getText().toString();
+                    hour = timePicker.getHour();
+                    minute = timePicker.getMinute();
+                    Snackbar.make(root, "Secret :" + eventText
+                            + " @" + year + "." + month + "." + day + "." + hour + ":" + minute
+                            + " @" + locationText
+                            , Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
                 //这里实现volley
--- FrontEnd/app/src/main/java/com/cs309/cychedule/activities/ui/dayscounter/DaysCounterFragment.java
+++ FrontEnd/app/src/main/java/com/cs309/cychedule/activities/ui/dayscounter/DaysCounterFragment.java
@@ -8,18 +8,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.support.annotation.Nullable;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.widget.Toast;
 
 import com.cs309.cychedule.R;
-import com.cs309.cychedule.activities.ui.calendar.CalendarFragment;
 
-import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -48,7 +43,7 @@ public class DaysCounterFragment extends Fragment {
         // });
 
 
-        final EditText secretInput = root.findViewById(R.id.et_material_design);
+        final EditText secretInput = root.findViewById(R.id.secretTextInput);
 
         final DatePicker datePicker = root.findViewById(R.id.datepicker_dayscounter);
         Calendar calendar = Calendar.getInstance();
