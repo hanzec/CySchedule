fd91e662287f0899f2ede772424c303be18c6223
Hanze Chen <tony.chen424@gmail.com>
2019-10-27 22:49:04 -0500

    code update

--- BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/security/filter/JwtTokenFilter.java
+++ BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/security/filter/JwtTokenFilter.java
@@ -1,7 +1,10 @@
 package edu.iastate.coms309.cyschedulebackend.security.filter;
 
+import com.google.gson.Gson;
 import edu.iastate.coms309.cyschedulebackend.Service.AccountService;
 import edu.iastate.coms309.cyschedulebackend.Service.UserTokenService;
+import edu.iastate.coms309.cyschedulebackend.persistence.model.Response;
+import edu.iastate.coms309.cyschedulebackend.persistence.model.UserCredential;
 import edu.iastate.coms309.cyschedulebackend.persistence.model.UserLoginToken;
 import edu.iastate.coms309.cyschedulebackend.security.model.TokenObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +22,9 @@ import java.io.IOException;
 public class JwtTokenFilter extends GenericFilterBean {
 
     @Autowired
+    Gson gson;
+
+    @Autowired
     AccountService accountService;
 
     @Autowired
@@ -30,18 +36,22 @@ public class JwtTokenFilter extends GenericFilterBean {
         HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
         try {
             TokenObject userLoginToken = userTokenService.load(httpServletRequest.getHeader("JwtToken"));
+            UserDetails userDetails = accountService.loadUserByUsername(accountService.getUserEmail(userLoginToken.getUserID()));
 
-            if (userLoginToken != null && userTokenService.verify(userLoginToken)) {
-                String userId = userLoginToken.getUserID();
-
-                UserDetails userDetails = accountService.loadUserByUsername(accountService.getUserEmail(userId));
+            if (userTokenService.verify(userLoginToken, (UserCredential) userDetails)) {
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpServletRequest));
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
+            Response response = new Response();
             SecurityContextHolder.clearContext();
-            httpServletResponse.sendError(403,ex.getMessage());
+            servletResponse.getWriter().write(
+                    gson.toJson(
+                            response
+                                    .BadRequested("Not include Enough Information")
+                                    .send(((HttpServletRequest) servletRequest).getRequestURI())
+                                    .addResponse("Error Item", ex.getMessage())));
             logger.error("Could not set user authentication in security context", ex);
         }
 
