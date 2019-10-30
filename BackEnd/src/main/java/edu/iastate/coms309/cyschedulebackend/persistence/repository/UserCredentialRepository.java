a5ac578a0a050fee801b9cad5ae693a869c36d9a
Hanze Chen <tony.chen424@gmail.com>
2019-10-30 10:59:35 -0500

    fix_authentication

--- BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/persistence/repository/UserCredentialRepository.java
+++ BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/persistence/repository/UserCredentialRepository.java
@@ -2,9 +2,12 @@ package edu.iastate.coms309.cyschedulebackend.persistence.repository;
 
 import edu.iastate.coms309.cyschedulebackend.persistence.model.UserCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserCredentialRepository extends JpaRepository<UserCredential, String> {
 
+    @Query("SELECT email FROM UserCredential a WHERE a.userID = ?1")
+    String getUserEmailByUserID(String id);
 }
