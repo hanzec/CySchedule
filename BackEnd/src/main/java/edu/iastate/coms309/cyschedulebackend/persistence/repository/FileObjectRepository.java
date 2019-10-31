daa3a4ae36b7d5642f21119af0447bb86f5e9656
Hanze Chen <tony.chen424@gmail.com>
2019-10-31 02:05:23 -0500

    fix bug

--- BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/persistence/repository/FileObjectRepository.java
+++ BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/persistence/repository/FileObjectRepository.java
@@ -2,6 +2,8 @@ package edu.iastate.coms309.cyschedulebackend.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import edu.iastate.coms309.cyschedulebackend.persistence.model.FileObject;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FileObjectRepository extends JpaRepository<FileObject,String> {
 }
