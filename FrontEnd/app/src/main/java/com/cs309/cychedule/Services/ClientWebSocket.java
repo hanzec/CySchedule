6a7a355d1953eaf4ed4ed690d83c1656afffbddf
Xingting Lai <xlai@iastate.edu>
2019-11-04 23:10:46 -0600

    Add a WebSocket at client side

---
+++ FrontEnd/app/src/main/java/com/cs309/cychedule/Services/ClientWebSocket.java
@@ -0,0 +1,45 @@
+package com.cs309.cychedule.Services;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class ClientWebSocket extends WebSocketClient {
+
+    public ClientWebSocket(URI serverUri, Draft draft) {
+        super(serverUri, draft);
+    }
+
+    public ClientWebSocket(URI serverURI) {
+        super(serverURI);
+    }
+
+    @Override
+    public void onOpen(ServerHandshake handshake) {
+        send("Connection Request");
+        System.out.println("New connection has opened.");
+    }
+
+    @Override
+    public void onClose(int code, String reason, boolean remote) {
+        System.out.println("Connection has closed because of: " + reason);
+    }
+
+    @Override
+    public void onMessage(String message) {
+        System.out.println("Received message: " + message);
+    }
+
+    @Override
+    public void onError(Exception e) {
+        System.err.println("Exception:" + e.toString());
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        WebSocketClient client = new ClientWebSocket(new URI(""));
+        client.connect();
+    }
+}
---
+++ FrontEnd/app/src/main/java/com/cs309/cychedule/activities/WebSocketActivity.java
@@ -0,0 +1,124 @@
+package com.cs309.cychedule.activities;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.cs309.cychedule.R;
+
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketActivity extends Activity {
+    private WebSocketClient webSocketClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_websocket);
+
+        connectWebSocket();
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_websocket, container, false);
+            return rootView;
+        }
+    }
+
+    private void connectWebSocket() {
+        URI uri;
+        try {
+            uri = new URI("");
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        webSocketClient = new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake serverHandshake) {
+                Log.i("Websocket", "Opened");
+                webSocketClient.send("Hello from " + Build.MANUFACTURER + " " + Build.MODEL);
+            }
+
+            @Override
+            public void onMessage(String s) {
+                final String message = s;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView textView = (TextView)findViewById(R.id.messages);
+                        textView.setText(textView.getText() + "\n" + message);
+                    }
+                });
+            }
+
+            @Override
+            public void onClose(int i, String s, boolean b) {
+                Log.i("Websocket", "Closed " + s);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.i("Websocket", "Error " + e.getMessage());
+            }
+        };
+        webSocketClient.connect();
+    }
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText)findViewById(R.id.message);
+        webSocketClient.send(editText.getText().toString());
+        editText.setText("");
+    }
+}
