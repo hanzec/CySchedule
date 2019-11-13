4a1d6ab195f957ebccca2bac521150a0d78b88c4
Hanze Chen <tony.chen424@gmail.com>
2019-11-13 00:14:55 -0600

    code update for support java.sql.date

--- BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/persistence/requestModel/EventRequest.java
+++ BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/persistence/requestModel/EventRequest.java
@@ -2,6 +2,9 @@ package edu.iastate.coms309.cyschedulebackend.persistence.requestModel;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.sql.Date;
@@ -11,9 +14,11 @@ public class EventRequest {
     @NotBlank(message = "Event should contains a name")
     public String name;
 
+    @Temporal(TemporalType.DATE)
     @NotNull(message = "Event should contains a endTime")
     public Date endTime;
 
+    @Temporal(TemporalType.DATE)
     @NotNull(message = "Event should contains a startTime")
     public Date startTime;
 
