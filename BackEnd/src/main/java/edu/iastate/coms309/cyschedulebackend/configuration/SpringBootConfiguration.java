07fbd2c2f42afd023d0531670169bc6d61c6d04c
Hanze Chen <tony.chen424@gmail.com>
2019-11-22 15:19:30 -0600

    enable Transaction Management for project

--- BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/configuration/SpringBootConfiguration.java
+++ BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/configuration/SpringBootConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -22,6 +23,7 @@ import java.time.ZonedDateTime;
 @Configuration
 @EnableCaching
 @EnableSwagger2
+@EnableTransactionManagement
 public class SpringBootConfiguration {
 
     @Bean
