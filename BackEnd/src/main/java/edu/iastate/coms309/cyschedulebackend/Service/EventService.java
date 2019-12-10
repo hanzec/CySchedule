ecad4120e2d8982a16d5b855efde33ecbed9bea9
Hanze Chen <tony.chen424@gmail.com>
2019-12-10 15:40:15 -0600

    fix get event

--- BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/Service/EventService.java
+++ BackEnd/src/main/java/edu/iastate/coms309/cyschedulebackend/Service/EventService.java
@@ -73,6 +73,7 @@ public class EventService {
         event.setDescription(newEvent.getDescription());
 
         //set relation with user
+        event.getRelatedUser().add(userInformation);
         event.getAdminUser().getManagedEvent().add(event);
 
         eventRepository.save(event);
@@ -116,19 +117,10 @@ public class EventService {
         return eventRepository.getOne(eventID).getAdminUser().getUserID().equals(userID);
     }
 
+    @Transactional
+    public List<Event> getAllEvent(String userID) { return eventRepository.getAllEvent(userID); }
 
     @Transactional
-    public List<Event> getAllManagedEvent(String userID){
-        return eventRepository.getManagedEvent(userID);
-    }
-
-    @Transactional
-    public List<Event> getAllEvent(String userID) {
-        List<Event> events = new ArrayList<>();
-
-        events.addAll(eventRepository.getJoinedEvent(userID));
-        events.addAll(eventRepository.getManagedEvent(userID));
-        return events;
-    }
+    public List<Event> getAllManagedEvent(String userID){ return eventRepository.getAdminEvent(userID); }
 
 }
